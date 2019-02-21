3a3e948ea1c4687c234c9f2b2850c8a3d5333a1c
Alexander Sahin <alxsah@Alexs-MBP.broadband>
2019-02-21 22:11:17 +0000

    fix indentation

--- booking/booking.go
+++ booking/booking.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Booking struct {
-  ID		bson.ObjectId 	`bson:"_id" json:"id"`
+  ID    bson.ObjectId   `bson:"_id" json:"id"`
   Name 	string 			    `bson:"name" json:"name"`
   Date	string			    `bson:"date" json:"date"`
 }
